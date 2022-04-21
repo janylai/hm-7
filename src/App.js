3ab83e9211eedfb241b109aa54bc33adaf62d4e5
janylai <janylai.momunalieva.99@bk.ru>
2022-04-21 07:58:26 -0500

    hm-8

--- src/App.js
+++ src/App.js
@@ -2,24 +2,48 @@ import Comment from './components/Comment';
 import './App.css';
 
 
-
-
-const comment = {
-  date: new Date(),
-  text: 'I hope you enjoy learning React!',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'http://placekitten.com/g/64/64',
-  }
-};
-
-
 function App() {
+
+  const comment = [
+    {
+      date: new Date(),
+      text: 'I hope you enjoy learning React!',
+      author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'http://placekitten.com/g/62/62',
+      },
+    },
+    {
+      date: new Date(),
+      text: 'I hope you enjoy learning React!',
+      author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'http://placekitten.com/g/64/64',
+      },
+    },
+    {
+      date: new Date(),
+      text: 'I hope you enjoy learning React!',
+      author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'http://placekitten.com/g/64/64',
+      },
+    },
+  ];
+  
   return (
-   < Comment 
-   author ={comment.author}
-    text={comment.text} 
-    date={comment.date}/>
+    <div className='App'>
+       {comment.map((element) => {
+         return(
+          < Comment key={element.id}
+          author ={element.author}
+           text={element.text} 
+           date={element.date}/>
+         )
+       })}
+  
+    </div>
+
     
   )
  
